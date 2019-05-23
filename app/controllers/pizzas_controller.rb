a328fcdd673190859eecc2b52ffa3785490bcd11
Samidh Patel <spatel1345@gmail.com>
2019-05-23 17:39:19 -0400

    finished with the controllers

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,2 +1,119 @@
 class PizzasController < ApplicationController
+
+    def index 
+        @pizzas = Pizza.all
+        render json: @pizzas, status: :ok
+    end 
+
+    def show 
+        @pizza = Pizza.find(params[:id])
+        render json: @pizza, status: :ok
+    end
+
+    def create 
+        @pizza = Pizza.new(pizza_params)
+        if @pizza.save
+            render json: @pizza, status: :created
+        else
+            render json: {errors: @pizza.errors.full_messages}, status: :unporcessable_entity
+        end 
+    end 
+
+    def update
+    end 
+
+    def delete
+        @pizza = Pizza.find(params[:id])
+        @pizza.destroy
+        render json: {}, status: :no_content 
+    end 
+
+    private 
+
+    def pizza_params 
+        params.permit(:name, :topping1, :topping2, :topping3, :total, :status, :order_id)
+    end 
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,38 @@
 class UsersController < ApplicationController
 
-    has_many :orders
-    has_many :pizzas, through: :orders    
+    def index 
+        @users = User.all
+        render json: @users, status: :ok
+    end 
+
+    def show 
+        @user = User.find(params[:id])
+        render json: @user, status: :ok
+    end
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user, status: :created
+        else
+            render json: {errors: @user.errors.full_messages}, status: :unporcessable_entity
+        end 
+    end 
+
+    def update
+    end 
+
+    def delete
+        @user = User.find(params[:id])
+        @user.destroy
+        render json: {}, status: :no_content 
+    end 
+
+    private 
+
+    def user_params 
+        params.permit(:name, :email, :password_digest, :address, :phone, :order_id)
+    end 
+
 
 end
